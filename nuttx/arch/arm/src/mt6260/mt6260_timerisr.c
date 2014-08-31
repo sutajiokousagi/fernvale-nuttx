0ba710dd2d41d4ed033c672ce6ed699aba83c75f
Sean Cross <xobs@kosagi.com>
2014-08-31 23:04:09 +0800

    mt6260: timerisr: Get system timer working
    We now have a basic system timer running off GPT1.  It runs at a
    base rate of 16 kHz, and gets divided by the number of ticks per second.

--- nuttx/arch/arm/src/mt6260/mt6260_timerisr.c
+++ nuttx/arch/arm/src/mt6260/mt6260_timerisr.c
@@ -48,6 +48,7 @@
 #include "up_arch.h"
 #include "clock/clock.h"
 #include "up_internal.h"
+#include "mt6260_gpt.h"
 
 /****************************************************************************
  * Pre-processor Definitions
@@ -55,46 +56,7 @@
 
 /* MT6260 Timers
  *
- * Each of the general-purpose timers can run in one of two modes:  one-
- * shot mode and free-run mode.  In one-shot mode, an interrupt only
- * occurs once and then the timer must be explicitly reset to begin the
- * timing operation again.  In free-run mode, when the timer generates an
- * interrupt, the timer counter is automatically reloaded to start the count
- * operation again.  Use the bit field MODE in TMMDx to configure the
- * timer for one-shot more or free-run mode. The bit field MODE in TMMDx
- * also allows you to stop the timer.
- *
- * Either the ARM clock divided by 2 (CLK_ARM/2) or an external clock
- * connected to the M27XI pin can be selected as the clock source of the
- * timer.
- *
- * The actual clock frequency used in the timer count operation is the input
- * clock divided by: 1 plus the value set in the bit field PRSCL of the
- * register TMPRSCLx (10 bits).  The timer expires when it reaches the
- * value set in the bit field DIV of the register TMDIVx (16 bits) plus 1.
- * PRSCL+1 is the source clock frequency divide factor and DIV+1 is the
- * timer count value.  The frequency of a timer interrupt is given by the
- * following equation:
- *
- * Interrupt Frequency = (Source Clock Frequency) / (PRSCL+1) / (DIV+1)
  */
-
-/* System Timer
- *
- * Timer0 is dedicated as the system timer.  The rate of system timer
- * interrupts is assumed to to 10MS per tick / 100Hz. The following
- * register settings are used for timer 0
- *
- * System clock formula:
- *   Interrupt Frequency = (Source Clock Frequency) / (PRSCL+1) / (DIV+1)
- *   Source Clock Frequency = 27MHz  (PLL clock)
- *   DIV                    = 26,999 (Yields 1Khz timer clock)
- *   PRSCL                  = 9      (Produces 100Hz interrupts)
- */
-
-#define MT6260_TMR0_MODE  MT6260_TMR_MODE_FREERUN /* Free running */
-#define MT6260_TMR0_DIV   26999                  /* (see above) */
-#define MT6260_TMR0_PRSCL 9                      /* (see above) */
 
 /****************************************************************************
  * Private Types
@@ -119,11 +81,11 @@
 
 int up_timerisr(int irq, uint32_t *regs)
 {
-#if 0
    /* Process timer interrupt */
+  uint32_t gpt_num = gpt_read(GPT_STATUS);
 
-   sched_process_timer();
-#endif
+  if (gpt_num == 1)
+    sched_process_timer();
    return 0;
 }
 
@@ -138,24 +100,23 @@ int up_timerisr(int irq, uint32_t *regs)
 
 void up_timer_initialize(void)
 {
-#if 0
-  up_disable_irq(MT6260_IRQ_SYSTIMER);
-
-  /* Start timer0 running so that an interrupt is generated at
-   * the rate USEC_PER_TICK.
-   */
-
-  putreg16(MT6260_TMR0_PRSCL, MT6260_TIMER0_TMPRSCL); /* Timer 0 Prescalar */
-  putreg16(MT6260_TMR0_DIV, MT6260_TIMER0_TMDIV);     /* Timer 0 Divisor (count) */
-
+  
   /* Start the timer */
 
-  putreg16(MT6260_TMR0_MODE, MT6260_TIMER0_TMMD); /* Timer 0 Mode */
+  gpt_write(8192 / CLK_TCK, GPT_DAT1);
+  gpt_write(GPT_PRESCALE1_16KHZ, GPT_PRESCALE1);
+
+  /* Force readback, which seems necessary to get it activated */
+  gpt_read(GPT_PRESCALE1);
+  gpt_write(GPT_CON1_MODE_FREERUN | GPT_CON1_EN, GPT_CON1);
+  gpt_read(GPT_CON1);
+  gpt_write(GPT_CON1_MODE_FREERUN | GPT_CON1_EN, GPT_CON1);
+  gpt_read(GPT_CON1);
+
 
   /* Attach and enable the timer interrupt */
 
+  up_irq_level(MT6260_IRQ_SYSTIMER);
   irq_attach(MT6260_IRQ_SYSTIMER, (xcpt_t)up_timerisr);
   up_enable_irq(MT6260_IRQ_SYSTIMER);
-#endif
 }
-
